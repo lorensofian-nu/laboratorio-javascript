ab56cd98604fbadbb1e74d42f24488f6beb2d518
Loren nunez <lorensofian23@gmail.com>
2026-06-12 20:50:43 -0500

    feat:Acceder  a los elementos del documento...

---
+++ main.js
@@ -0,0 +1,8 @@
+const formulario = document.querySelector("#nombre")
+const nota1 = document.querySelector("#nota1")
+const nota2 = document.querySelector("#nota2")
+const nota3 = document.querySelector("#nota3")
+const boton = document.querySelector("#evaluar-nota")
+const resultado = document.querySelector("#resultado")
+
+boton.addEventListener("click")
